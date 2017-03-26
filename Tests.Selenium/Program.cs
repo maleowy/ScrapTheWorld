d184f5bc9aae328dcf25556cc902f05e7e1a1214
Maleowy <maleowy@gmail.com>
2017-03-26 19:22:21 +0200

    selenium tests

---
+++ Tests.Selenium/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests.Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://duckduckgo.com/");
+
+            IWebElement input = driver.FindElement(By.Id("search_form_input_homepage"));
+            input.SendKeys("Test");
+
+            IWebElement button = driver.FindElement(By.Id("search_button_homepage"));
+            button.Click();
+
+            System.Console.WriteLine("Page title is: " + driver.Title);
+            driver.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
