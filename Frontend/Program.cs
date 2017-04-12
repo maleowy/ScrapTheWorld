a3791b1a3b6c42b7daf15d77c4890779761173e2
Maleowy <maleowy@gmail.com>
2017-04-12 22:04:59 +0200

    cleanup

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -4,59 +4,32 @@ using System.Diagnostics;
 using EasyNetQ;
 using Models;
 using System.IO;
+using System.Net;
 using System.Threading;
 
 namespace Frontend
 {
     class Program
     {
+        public static IBus Bus;
+
+        public static string Dir;
+        public static string WorkerDir;
+        public static string WorkerExe;
+
         static void Main(string[] args)
         {
             Console.Title = "Frontend";
 
-            var dir = AppDomain.CurrentDomain.BaseDirectory;
-            dir = Path.GetFullPath(Path.Combine(dir, @"..\..\..\..\"));
+            PreparePaths();
+            AddRabbit();
 
-            var erl = Process.GetProcessesByName("erl");
-
-            if (erl.Length == 0 && Directory.Exists(Path.Combine(dir, @"RabbitMQ")))
-            {
-                if (!Directory.Exists(Path.Combine(dir, @"RabbitMQ\rabbitmq_server-3.6.6")))
-                {
-                    var extract = new Process
-                    {
-                        StartInfo =
-                        {
-                            FileName = Path.Combine(dir, "RabbitMQ", "extract.bat"),
-                            WorkingDirectory = Path.Combine(dir, "RabbitMQ")
-                        }
-                    };
-                    extract.Start();
-                    extract.WaitForExit();
-                }
-
-                var process = new Process
-                {
-                    StartInfo =
-                    {
-                        FileName = Path.Combine(dir, "RabbitMQ", "start.bat"),
-                        WorkingDirectory = Path.Combine(dir, "RabbitMQ")
-                    }
-                };
-                process.Start();
-
-                Thread.Sleep(20*1000);
-            }
-
-            IBus Bus = RabbitHutch.CreateBus("host=localhost");
+            Bus = RabbitHutch.CreateBus("host=localhost");
             Bus.Subscribe<Result>("subscriptionId", x => Console.WriteLine(x.Data));
 
             Console.WriteLine("o - offscreen, f - winforms, w - wpf, p - publish, esc - exit");
 
-            var workerDir = Path.Combine(dir, @"Worker.CefSharp.{0}\bin\x86\Debug");
-            var workerExe = Path.Combine(workerDir, @"Worker.CefSharp.{0}.exe");
-
-            var processes = new List<Process>();
+            var workers = new List<Process>();
 
             while (true)
             {
@@ -65,55 +38,120 @@ namespace Frontend
                 switch (key)
                 {
                     case ConsoleKey.O:
-                        var o = new Process {
-                            StartInfo =
-                            {
-                                FileName = string.Format(workerExe, "OffScreen"),
-                                WorkingDirectory = string.Format(workerDir, "OffScreen")
-                            }
-                        };
-                        processes.Add(o);
-                        o.Start();
+                        workers.Add(StartWorker("OffScreen"));
                         break;
                     case ConsoleKey.F:
-                        var f = new Process
-                        {
-                            StartInfo =
-                            {
-                                FileName = string.Format(workerExe, "WinForms"),
-                                WorkingDirectory = string.Format(workerDir, "WinForms")
-                            }
-                        };
-                        processes.Add(f);
-                        f.Start();
+                        workers.Add(StartWorker("WinForms"));
                         break;
                     case ConsoleKey.W:
-                        var w = new Process
-                        {
-                            StartInfo =
-                            {
-                                FileName = string.Format(workerExe, "WPF"),
-                                WorkingDirectory = string.Format(workerDir, "WPF")
-                            }
-                        };
-                        processes.Add(w);
-                        w.Start();
+                        workers.Add(StartWorker("WPF"));
                         break;
                     case ConsoleKey.P:
-                        Bus.Publish(new Node { Url = "http://www.wp.pl", Script = "document.title" });
-                        Bus.Publish(new Node { Url = "http://www.onet.pl", Script = "document.title" });
-                        Bus.Publish(new Node { Url = "http://www.interia.pl", Script = "document.title" });
+                        Publish();
                         break;
                     case ConsoleKey.Escape:
-                        processes.ForEach(p => p.Kill());
+                        workers.ForEach(p => p.Kill());
                         //erl.ToList().ForEach(e => e.Kill());
                         Environment.Exit(0);
                         break;
-
                 }
 
                 Console.WriteLine();
             }
+        }
+
+        private static void PreparePaths()
+        {
+            Dir = AppDomain.CurrentDomain.BaseDirectory;
+            Dir = Path.GetFullPath(Path.Combine(Dir, @"..\..\..\..\"));
+
+            WorkerDir = Path.Combine(Dir, @"Worker.CefSharp.{0}\bin\x86\Debug");
+            WorkerExe = Path.Combine(WorkerDir, @"Worker.CefSharp.{0}.exe");
+        }
+
+        private static void AddRabbit()
+        {
+            var erl = Process.GetProcessesByName("erl");
+
+            if (erl.Length == 0 && Directory.Exists(Path.Combine(Dir, @"RabbitMQ")))
+            {
+                if (!Directory.Exists(Path.Combine(Dir, @"RabbitMQ\rabbitmq_server-3.6.6")))
+                {
+                    ExtractRabbit();
+                }
+
+                StartRabbit();
+            }
+        }
+
+        private static Process StartWorker(string name)
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    FileName = string.Format(WorkerExe, name),
+                    WorkingDirectory = string.Format(WorkerDir, name)
+                }
+            };
+
+            p.Start();
+            return p;
+        }
+
+        private static void Publish()
+        {
+            Bus.Publish(new Node { Url = "http://www.wp.pl", Script = "document.title" });
+            Bus.Publish(new Node { Url = "http://www.onet.pl", Script = "document.title" });
+            Bus.Publish(new Node { Url = "http://www.interia.pl", Script = "document.title" });
+        }
+
+        private static void StartRabbit()
+        {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = Path.Combine(Dir, "RabbitMQ", "start.bat"),
+                    WorkingDirectory = Path.Combine(Dir, "RabbitMQ")
+                }
+            };
+            process.Start();
+
+            while (!UrlExists("http://localhost:15672"))
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static bool UrlExists(string url)
+        {
+            var req = WebRequest.Create(url);
+
+            try
+            {
+                req.GetResponse();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void ExtractRabbit()
+        {
+            var extract = new Process
+            {
+                StartInfo =
+                {
+                    FileName = Path.Combine(Dir, "RabbitMQ", "extract.bat"),
+                    WorkingDirectory = Path.Combine(Dir, "RabbitMQ")
+                }
+            };
+            extract.Start();
+            extract.WaitForExit();
         }
     }
 }
